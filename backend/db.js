b5409ec33e4465ac95228d55eb1e32f02555cec2
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-12-04 20:45:51 -0300

    installed dotenv and made modifications on db and index files for db connection

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require("sequelize");
 const NoteModel = require("./models/note");
 
-const sequelize = new Sequelize("tutorialdb", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 const Note = NoteModel(sequelize, Sequelize);
 sequelize.sync({ force: false }).then(() => {
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const apiRouter = require('./routes/notes.routes')
+const apiRouter = require("./routes/notes.routes");
 
 require("./db");
 
+app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3001"],
+  })
+);
 
-app.listen(3001, () => {
-  console.log("Server running!");
+app.listen(app.get("port"), () => {
+  console.log("Server running on port " + app.get("port"));
 });
-app.use('/api',apiRouter)
+app.use("/api", apiRouter);
