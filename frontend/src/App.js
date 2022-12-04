110daaee1141ac9d2331152172999b922fed478e
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-12-04 11:33:43 -0300

    edited app.js with routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import Home from "./components/views/Home";
+import Menu from "./components/common/Menu";
+import CreateNote from "./components/views/CreateNote";
+import EditNote from "./components/views/EditNote";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="App">
+      <BrowserRouter>
+        <Menu></Menu>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/createNote" element={<CreateNote />}></Route>
+          <Route exact path="/editNote" element={<EditNote />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
