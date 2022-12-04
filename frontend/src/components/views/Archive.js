2d4b970f79baa33ff58b344d20ca4d2373e237ef
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-12-04 20:52:40 -0300

    added archive.js to fetch all archive notes

--- frontend/src/components/views/Archive.js
+++ frontend/src/components/views/Archive.js
@@ -1,11 +1,39 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import axios from "../../axios";
+import { Row } from "react-bootstrap";
+import Note from "./Note";
 
 const Archive = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    consultApi();
+  }, []);
+
+  const consultApi = () => {
+    try {
+      axios.get("/api/archive").then((res) => {
+        setNotes(res.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
-    <div className="container mt-4">
-      <h3>Archived notes</h3>
-      <hr className="mt-0" />
-    </div>
+    <main className="container">
+      <section className="mt-4">
+        <h3>Archived notes</h3>
+        <hr />
+      </section>
+      <section className="mb-4">
+        <Row xs={1} md={3} className="g-4">
+          {notes.map((note) => (
+            <Note note={note} key={note._id} consultApi={consultApi}></Note>
+          ))}
+        </Row>
+      </section>
+    </main>
   );
 };
 
